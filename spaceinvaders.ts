1814e059d57dfc9a184471eac69f72373b1c1215
robertovers <oversr14@gmail.com>
2021-08-26 20:30:53 +1000

    Add a Class and observable streams for shooting

--- spaceinvaders.ts
+++ spaceinvaders.ts
@@ -2,23 +2,21 @@ import { fromEvent, interval, merge } from 'rxjs';
 import { map, filter, scan } from 'rxjs/operators';
 
 function spaceinvaders() {
-    // Inside this function you will use the classes and functions
-    // from rx.js
-    // to add visuals to the svg element in pong.html, animate them, and make them interactive.
-    // Study and complete the tasks in observable exampels first to get ideas.
-    // Course Notes showing Asteroids in FRP: https://tgdwyer.github.io/asteroids/
-    // You will be marked on your functional programming style
-    // as well as the functionality that you implement.
-    // Document your code!
+
+    const Constants = {
+        gameWidth: 600,
+        gameHeight: 600,
+    } as const;
 
     class Tick { constructor(public readonly elapsed: number)  { } }
     class MoveLeft { constructor(public readonly on: boolean) { } }
     class MoveRight { constructor(public readonly on: boolean) { } }
     class MouseMove { constructor(public readonly mousePos: {x: number, y: number}) { } }
+    class Shoot { constructor() { } }
 
-    type Event = 'keydown' | 'keyup' | 'mousemove'
-    type Key = 'ArrowLeft' | 'ArrowRight' | 'ArrowUp'
- 
+    type Event = 'keydown' | 'keyup' | 'mousemove';
+    type Key = 'ArrowLeft' | 'ArrowRight' | 'ArrowUp' | ' ';
+
     type State = Readonly<{
         x: number,
         y: number
@@ -31,8 +29,11 @@ function spaceinvaders() {
         vel: 0
     }
 
+    const canvas = document.getElementById('canvas')!;
+    const ship = document.getElementById('ship')!;
+
     const gameClock = interval(10)
-        .pipe(map(elapsed => new Tick(elapsed)))
+        .pipe(map(elapsed => new Tick(elapsed)));
 
     // from Asteroids
     const keyObservable = <T>(e: Event, k: Key, result: () => T) =>
@@ -40,29 +41,32 @@ function spaceinvaders() {
             .pipe(
                 filter(({ code }) => code === k),
                 filter(({ repeat }) => !repeat),
-                map(result))
+                map(result));
 
     const
         startMoveLeft = keyObservable('keydown', 'ArrowLeft', () => new MoveLeft(true)),
         stopMoveLeft = keyObservable('keyup', 'ArrowLeft', () => new MoveLeft(false)),
         startMoveRight = keyObservable('keydown', 'ArrowRight', () => new MoveRight(true)),
         stopMoveRight = keyObservable('keyup', 'ArrowRight', () => new MoveRight(false)),
-        mouseMove = fromEvent<MouseEvent>(document, 'mousemove').pipe(map(({ clientX, clientY }) => new MouseMove({ x: clientX, y: clientY })))
+        spacePress = keyObservable('keydown', ' ', () => new Shoot()),
+        mouseMove = fromEvent<MouseEvent>(document, 'mousemove').pipe(map(({ clientX, clientY }) => new MouseMove({ x: clientX, y: clientY }))),
+        mouseClick = fromEvent<MouseEvent>(document, 'mousedown').pipe(map(() => new Shoot()))
 
-    const reduceState = (s: State, e: Tick | MoveLeft | MoveRight | MouseMove) => 
+    
+    const reduceState = (s: State, e: Tick | MoveLeft | MoveRight | MouseMove | Shoot) =>
         e instanceof MouseMove ? {...s,
             x: e.mousePos.x,
         } :
-        e instanceof MoveLeft ? {...s, 
+        e instanceof MoveLeft ? {...s,
             vel: e.on ? -5 : 0
-        } : 
-        e instanceof MoveRight ? {...s, 
+        } :
+        e instanceof MoveRight ? {...s,
             vel: e.on ? 5 : 0
         } : {...s,
-            x: s.x + s.vel    
+            x: s.x + s.vel
         };
 
-    const subscription = 
+    const subscription =
         merge(
             gameClock,
             mouseMove,
@@ -72,15 +76,13 @@ function spaceinvaders() {
         .pipe(
             scan(reduceState, initialState)
         )
-        .subscribe(updateView)
-    
+        .subscribe(updateView);
+
     function updateView(s: State) {
-        const ship = document.getElementById("ship")!
         ship.setAttribute('transform', `translate(${s.x},${s.y})`)
     }
 }
 
-// the following simply runs your pong function on window load.  Make sure to leave it in place.
 if (typeof window != 'undefined')
     window.onload = () => {
         spaceinvaders();
