50c006e5f6ff67f54cfe8adc5e787a09481a3c47
robertovers <oversr14@gmail.com>
2021-09-05 18:16:49 +1000

    Add enemy shooting from bottom row

--- spaceinvaders.ts
+++ spaceinvaders.ts
@@ -10,12 +10,12 @@ function spaceinvaders() {
         initialY: 560
     } as const;
 
-    class Tick { constructor(public readonly elapsed: number)  { } }
+    class Tick { constructor(public readonly elapsed: number) { } }
     class MoveLeft { constructor(public readonly on: boolean) { } }
     class MoveRight { constructor(public readonly on: boolean) { } }
-    class MouseMove { constructor(public readonly mousePos: {x: number, y: number}) { } }
+    class MouseMove { constructor(public readonly mousePos: { x: number, y: number }) { } }
     class PlayerShoot { constructor() { } }
-    class EnemyShoot { constructor(public readonly on: boolean) { } }
+    class EnemyShoot { constructor() { } }
 
     type Event = 'keydown' | 'keyup' | 'mousemove' | 'mousedown';
     type Key = 'ArrowLeft' | 'ArrowRight' | 'ArrowUp';
@@ -30,7 +30,8 @@ function spaceinvaders() {
 
     interface IEnemy extends GameObject {
         col: number,
-        row: number
+        row: number,
+        canShoot: boolean
     }
 
     interface IPlayer extends GameObject {
@@ -42,9 +43,9 @@ function spaceinvaders() {
     type Bullet = Readonly<GameObject>
 
     type Enemy = Readonly<IEnemy>
-    
+
     interface IEnemies extends GameObject {
-        enemies: ReadonlyArray<Enemy> 
+        enemies: ReadonlyArray<Enemy>
     }
 
     type EnemyTracker = Readonly<IEnemies>
@@ -57,24 +58,25 @@ function spaceinvaders() {
         exit: ReadonlyArray<GameObject>
     }>
 
-    const 
-        enemyCols = [1,2,3,4,5,6,7,8,9,10,11],
-        enemyRows = [1,2,3,4,5],
+    const
+        enemyCols = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+        enemyRows = [1, 2, 3, 4, 5],
         enemyRowCols = enemyRows.flatMap(a => enemyCols.map(b => [a, b]));
 
-    const initEnemies = () => enemyRowCols.map(coords => 
+    const initEnemies = () => enemyRowCols.map(coords =>
         <Enemy>{
             id: `enemy${coords[0]}${coords[1]}`,
             x: coords[1] * 40,
             y: coords[0] * 40,
             col: coords[1],
-            row: coords[0] 
+            row: coords[0],
+            canShoot: coords[0] === 5 ? true : false
         });
 
     const initialState: State = {
-        player: { 
+        player: {
             id: 'ship',
-            x: Constants.initialX, 
+            x: Constants.initialX,
             y: Constants.initialY,
             velX: 0,
             velY: 0,
@@ -93,11 +95,13 @@ function spaceinvaders() {
         exit: []
     };
 
-    const 
+    const rng = new RNG(20);
+
+    // html elements
+    const
         canvas = document.getElementById('canvas')!,
         ship = document.getElementById('ship')!,
         canvasRect = canvas.getBoundingClientRect();
-        
 
     // from Observable Asteroids
     const keyObservable = <T>(e: Event, k: Key, result: () => T) =>
@@ -106,15 +110,14 @@ function spaceinvaders() {
                 filter(({ code }) => code === k),
                 filter(({ repeat }) => !repeat),
                 map(result));
-    
-    const mouseOnCanvas = ({ clientX, clientY }: MouseEvent) => 
+
+    const mouseOnCanvas = ({ clientX, clientY }: MouseEvent) =>
         clientX > canvasRect.left &&
         clientX < canvasRect.right &&
         clientY > canvasRect.top &&
         clientY < canvasRect.bottom;
 
-    const nextFloat = () => new RNG(20).nextFloat();
-    
+    // observable streams
     const
         startMoveLeft = keyObservable('keydown', 'ArrowLeft', () => new MoveLeft(true)),
         stopMoveLeft = keyObservable('keyup', 'ArrowLeft', () => new MoveLeft(false)),
@@ -128,70 +131,75 @@ function spaceinvaders() {
             filter(mouseOnCanvas),
             map(({ clientX, clientY }) => new MouseMove({ x: clientX, y: clientY }))),
         gameClock = interval(10).pipe(map(elapsed => new Tick(elapsed))),
-        rngStream = interval(10).pipe(map(() => nextFloat() < 0.1 ? new EnemyShoot(true) : new EnemyShoot(false)));
-        
-    const movePlayer = (p: Player) => <Player>{
-        ...p,
-        x: p.x + p.velX,
-    };
+        enemyShootStream = interval(2000).pipe(map(() => new EnemyShoot()));
 
-    const moveBullet = (b: Bullet) => <GameObject>{
-        ...b,
-        y: b.y + b.velY
-    };
-
-    const moveEnemy = (et: EnemyTracker) => (e: Enemy) => <Enemy>{
-        ...e,
-        x: et.x + e.col * 40,
-        y: et.y + e.row * 40
-    };
-
-    const moveEnemies = (et: EnemyTracker) => <EnemyTracker>{
-        ...et,
-        x: et.x > 90 ? 89 : et.x < 10 ? 11 : et.x + et.velX,
-        y: et.y + et.velY,
-        velX: et.x > 90 || et.x < 10 ? (-1) * et.velX : et.velX, 
-        enemies: et.enemies.map(moveEnemy(et))
-    };
-
-    const newPlayerBullet = (s: State) => 
-        <GameObject>{
-            id: `bullet${s.objCount}`,
-            x: s.player.x - 1,
-            y: s.player.y - 15,
-            velX: 0,
-            velY: -5
+    const
+        movePlayer = (p: Player) => <Player>{
+            ...p,
+            x: p.x + p.velX,
+        },
+        moveBullet = (b: Bullet) => <GameObject>{
+            ...b,
+            y: b.y + b.velY
+        },
+        moveEnemy = (et: EnemyTracker) => (e: Enemy) => <Enemy>{
+            ...e,
+            x: et.x + e.col * 40,
+            y: et.y + e.row * 40
+        },
+        moveEnemies = (et: EnemyTracker) => <EnemyTracker>{
+            ...et,
+            x: et.x > 90 ? 89 : et.x < 10 ? 11 : et.x + et.velX,
+            y: et.y + et.velY,
+            velX: et.x > 90 || et.x < 10 ? (-1) * et.velX : et.velX,
+            enemies: et.enemies.map(moveEnemy(et))
         };
 
-    const newEnemyBullet = (s: State) =>
-        <GameObject>{
-            id: `bullet${s.objCount}`,
-            x: s.player.x + 10,
-            y: s.player.y + 30,
-            velX: 0,
-            velY: 5
-        };
+    const randEnemyThatShoots = (s: State) => {
+        const enemiesThatShoot = s.enemyTracker.enemies.filter(e => e.canShoot === true);
+        const randEnemy = rng.nextInt() % (enemiesThatShoot.length);
+        return enemiesThatShoot.length > 0 ? enemiesThatShoot[randEnemy] : s.enemyTracker.enemies[0];
+    }
+
+    const
+        newPlayerBullet = (s: State) =>
+            <GameObject>{
+                id: `bullet${s.objCount}`,
+                x: s.player.x - 1,
+                y: s.player.y - 15,
+                velX: 0,
+                velY: -5
+            },
+        newEnemyBullet = (s: State) =>
+            <GameObject>{
+                id: `bullet${s.objCount}`,
+                x: randEnemyThatShoots(s).x + 10,
+                y: randEnemyThatShoots(s).y + 20,
+                velX: 0,
+                velY: 3
+            };
 
     const bulletOnCanvas = (b: Bullet) =>
         b.x <= Constants.gameWidth &&
         b.y <= Constants.gameHeight &&
         b.x >= 0 &&
         b.y + 20 >= 0;
-    
+
     const handleCollisions = (s: State) => {
-        const objectCollision = ([i, j]: [GameObject, GameObject]) => 
-                i.x > j.x &&
-                i.x < j.x + 20 &&
-                i.y > j.y &&
-                i.y < j.y + 20;
+        const objectCollision = ([i, j]: [GameObject, GameObject]) =>
+            i.x > j.x &&
+            i.x < j.x + 20 &&
+            i.y > j.y &&
+            i.y < j.y + 20;
         const
             // from Observable Asteroids
-            allBulletsAndEnemies = s.bullets.flatMap(b => s.enemyTracker.enemies.map(e =><[GameObject, GameObject]>[b, e])),
+            allBulletsAndEnemies = s.bullets.flatMap(b => s.enemyTracker.enemies.map(e => <[GameObject, GameObject]>[b, e])),
             collided = allBulletsAndEnemies.filter(objectCollision),
             collidedBullets = collided.map(([bullet, _]) => bullet),
             collidedEnemies = collided.map(([_, enemy]) => enemy),
             cut = except((a: GameObject | Enemy) => (b: GameObject | Enemy) => a.id === b.id);
-        return <State>{...s,
+        return <State>{
+            ...s,
             bullets: cut(s.bullets)(collidedBullets),
             exit: s.exit.concat(collidedBullets, collidedEnemies),
             enemyTracker: {
@@ -202,11 +210,11 @@ function spaceinvaders() {
     }
 
     const tick = (s: State, elapsed: number) => {
-        const 
+        const
             offCanvasBullets = s.bullets.filter(b => !bulletOnCanvas(b)),
             onCanvasBullets = s.bullets.filter(bulletOnCanvas);
         return handleCollisions({
-            ...s, 
+            ...s,
             player: movePlayer(s.player),
             bullets: onCanvasBullets.map(moveBullet),
             enemyTracker: moveEnemies(s.enemyTracker),
@@ -214,34 +222,44 @@ function spaceinvaders() {
         })
     };
 
-    const reduceState = (s: State, e: MouseMove | MoveLeft | MoveRight | PlayerShoot | Tick) =>
-        e instanceof MouseMove ? <State>{...s,
-            player: {...s.player, x: e.mousePos.x - 10},
+    const reduceState = (s: State, e: MouseMove | MoveLeft | MoveRight | PlayerShoot | EnemyShoot | Tick) =>
+        e instanceof MouseMove ? <State>{
+            ...s,
+            player: { ...s.player, x: e.mousePos.x - 10 },
         } :
-        e instanceof MoveLeft ? <State>{...s,
-            player: {...s.player, velX: e.on ? -5 : 0},
-        } :
-        e instanceof MoveRight ? <State>{...s,
-            player: {...s.player, velX: e.on ? 5 : 0}, 
-        } : 
-        e instanceof PlayerShoot ? <State>{...s,
-            bullets: s.bullets.concat(newPlayerBullet(s)),
-            objCount: s.objCount + 1
-        }
-        : tick(s, e.elapsed);
+            e instanceof MoveLeft ? <State>{
+                ...s,
+                player: { ...s.player, velX: e.on ? -5 : 0 },
+            } :
+                e instanceof MoveRight ? <State>{
+                    ...s,
+                    player: { ...s.player, velX: e.on ? 5 : 0 },
+                } :
+                    e instanceof PlayerShoot ? <State>{
+                        ...s,
+                        bullets: s.bullets.concat(newPlayerBullet(s)),
+                        objCount: s.objCount + 1
+                    } :
+                        e instanceof EnemyShoot ? <State>{
+                            ...s,
+                            bullets: s.bullets.concat(newEnemyBullet(s)),
+                            objCount: s.objCount + 1
+                        }
+                            : tick(s, e.elapsed);
 
     const subscription =
         merge(
             gameClock,
+            enemyShootStream,
             mouseMove, mouseClick,
             startMoveLeft, stopMoveLeft,
             startMoveRight, stopMoveRight,
             spacePress
         )
-        .pipe(
-            scan(reduceState, initialState)
-        )
-        .subscribe(updateView);
+            .pipe(
+                scan(reduceState, initialState)
+            )
+            .subscribe(updateView);
 
     function updateView(s: State) {
         // from Observable Asteroids
@@ -277,14 +295,14 @@ function spaceinvaders() {
             v.setAttribute('y', String(e!.y));
         });
         s.exit.map(o => document.getElementById(o.id))
-        .filter(o => o !== null && o !== undefined)
-        .forEach(v => {
-            try {
-                canvas.removeChild(v!)
-            } catch(e) {
-                console.log("Already removed: " + v!.id)
-            }
-        });
+            .filter(o => o !== null && o !== undefined)
+            .forEach(v => {
+                try {
+                    canvas.removeChild(v!)
+                } catch (e) {
+                    console.log("Already removed: " + v!.id)
+                }
+            });
     }
 }
 
