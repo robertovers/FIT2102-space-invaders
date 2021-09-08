eac10aca21a73417124649a7386d67f594d967a2
robertovers <oversr14@gmail.com>
2021-09-08 16:52:59 +1000

    Add levels and enemies moving downwards

--- spaceinvaders.ts
+++ spaceinvaders.ts
@@ -7,13 +7,16 @@ function spaceinvaders() {
     const Constants = {
         GAME_WIDTH: 600,
         GAME_HEIGHT: 600,
-        INITIAL_X: 300,
-        INITIAL_Y: 550,
+        PLAYER_INITIAL_X: 300,
+        PLAYER_INITIAL_Y: 550,
         PLAYER_WIDTH: 40,
         PLAYER_HEIGHT: 40,
         ENEMY_WIDTH: 30,
         ENEMY_HEIGHT: 30,
-        ENEMY_SPACING: 40
+        ENEMY_SPACING: 40,
+        DOWN_STEP_TIME: 500,
+        ET_INITIAL_X: 5,
+        ET_INITIAL_Y: 40
     } as const;
 
     // classes
@@ -63,7 +66,8 @@ function spaceinvaders() {
         enemyTracker: EnemyTracker,
         objCount: number,
         exit: ReadonlyArray<GameObject>,
-        score: number
+        score: number,
+        level: number
     }>
 
     const
@@ -74,8 +78,8 @@ function spaceinvaders() {
     const initEnemies = () => enemyRowCols.map(coords =>
         <Enemy>{
             id: `enemy${coords[0]}${coords[1]}`,
-            x: coords[1] * Constants.ENEMY_SPACING,
-            y: coords[0] * Constants.ENEMY_SPACING,
+            x: coords[1] * Constants.ENEMY_SPACING + Constants.ET_INITIAL_X,
+            y: coords[0] * Constants.ENEMY_SPACING + Constants.ET_INITIAL_Y,
             col: coords[1],
             row: coords[0],
             canShoot: coords[0] === 5 ? true : false
@@ -84,8 +88,8 @@ function spaceinvaders() {
     const initialState: State = {
         player: {
             id: 'ship',
-            x: Constants.INITIAL_X,
-            y: Constants.INITIAL_Y,
+            x: Constants.PLAYER_INITIAL_X,
+            y: Constants.PLAYER_INITIAL_Y,
             velX: 0,
             velY: 0,
             status: 0
@@ -93,15 +97,16 @@ function spaceinvaders() {
         bullets: [],
         enemyTracker: {
             id: 'enemyTracker',
-            x: 5,
-            y: 20,
+            x: Constants.ET_INITIAL_X,
+            y: Constants.ET_INITIAL_Y,
             velX: 0.3,
-            velY: 0,
+            velY: 0.6,
             enemies: initEnemies()
         },
         objCount: 0,
         exit: [],
-        score: 0
+        score: 0,
+        level: 1
     };
 
     const rng = new RNG(20);
@@ -178,10 +183,13 @@ function spaceinvaders() {
      * @param et 
      * @returns 
      */
-    const moveEnemies = (et: EnemyTracker) => <EnemyTracker>{
+    const moveEnemies = (et: EnemyTracker, elapsed: number) => <EnemyTracker>{
         ...et,
         x: et.x > 90 ? 89 : et.x < 10 ? 11 : et.x + et.velX,
-        y: et.y + et.velY,
+        y: elapsed > Constants.DOWN_STEP_TIME
+            && elapsed % Constants.DOWN_STEP_TIME > 0
+            && elapsed % Constants.DOWN_STEP_TIME < 20
+            ? et.y + et.velY : et.y,
         velX: et.x > 90 || et.x < 10 ? (-1) * et.velX : et.velX,
         enemies: et.enemies.map(moveEnemy(et))
     };
@@ -272,17 +280,23 @@ function spaceinvaders() {
             cutEnemies = except((a: Enemy) => (b: Enemy) => a.id === b.id),
             enemiesInCol = (s: State, col: number) => s.enemyTracker.enemies.filter(e => e.col === col),
             lowerInCol = (e: Enemy, f: Enemy) => e.row > f.row ? e : f,
-            lowestInCol = (s: State, col: number) => enemiesInCol(s, col).reduce(lowerInCol);
+            lowestInCol = (s: State, col: number) => enemiesInCol(s, col).reduce(lowerInCol),
+            noEnemies = (s: State) => s.enemyTracker.enemies.length === 0;
         
         return <State>{
             ...s,
-            bullets: cutBullets(s.bullets)(collidedBullets),
-            exit: s.exit.concat(collidedBullets, collidedEnemies),
+            bullets: noEnemies(s) ? [] : cutBullets(s.bullets)(collidedBullets),
+            exit: noEnemies(s) ? s.exit.concat(s.bullets) : s.exit.concat(collidedBullets, collidedEnemies),
             enemyTracker: {
                 ...s.enemyTracker,
-                enemies: cutEnemies(s.enemyTracker.enemies.map(e => e.row === lowestInCol(s, e.col).row ? <Enemy>{...e, canShoot: true} : e))(collidedEnemies)
+                x: noEnemies(s) ? Constants.ET_INITIAL_X : s.enemyTracker.x,
+                y: noEnemies(s) ? Constants.ET_INITIAL_Y : s.enemyTracker.y,
+                enemies: noEnemies(s) ? initEnemies() 
+                    : cutEnemies(s.enemyTracker.enemies.map(e => 
+                    e.row === lowestInCol(s, e.col).row ? <Enemy>{...e, canShoot: true} : e))(collidedEnemies),
             },
-            score: s.score + collidedEnemies.length * 10
+            score: s.score + collidedEnemies.length * 10,
+            level: noEnemies(s) ? s.level + 1 : s.level
         }
     }
 
@@ -300,7 +314,7 @@ function spaceinvaders() {
             ...s,
             player: movePlayer(s.player),
             bullets: onCanvasBullets.map(moveBullet),
-            enemyTracker: moveEnemies(s.enemyTracker),
+            enemyTracker: moveEnemies(s.enemyTracker, elapsed),
             exit: offCanvasBullets
         })
     };
@@ -359,6 +373,7 @@ function spaceinvaders() {
      */
     function updateView(s: State) {
         document.getElementById('score')!.innerHTML = `score: ${String(s.score)}`;
+        document.getElementById('level')!.innerHTML = `level: ${String(s.level)}`;
         // from Observable Asteroids
         ship.setAttribute('transform', `translate(${s.player.x - Constants.PLAYER_WIDTH / 2},${s.player.y})`);
         s.bullets.forEach(b => {
